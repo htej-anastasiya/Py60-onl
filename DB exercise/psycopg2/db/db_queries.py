1d2eaa781e2163b244f6b427bce7b6f616999a2b
anastasiya-khtsei <khtsei.anastasiya@gmail.com>
2025-02-10 19:08:46 +0100

    select/delete/update queries plus main menu. Hometask l16

--- DB exercise/psycopg2/db/db_queries.py
+++ DB exercise/psycopg2/db/db_queries.py
@@ -10,7 +10,7 @@ def select_songs_from_album(query):
             JOIN artists art ON art.id = a.artist_id
             WHERE a.name ILIKE %s
         """
-        # print(f" SQL Запрос: {sql_query} с параметром: {query}")
+        # print(f" SQL query: {sql_query} с param: {query}")
         cursor.execute(sql_query,(f"%{query}%",))
         return cursor.fetchall()
 
@@ -29,11 +29,25 @@ def select_songs_by_name(query):
 
 def add_artist(name, country):
     with DbConnection() as cursor:
-        sql_query = """
-        Insert into artists (name, country) values (%s,%s);
-        """
-        cursor.execute(sql_query, (name, country))
-        print(f"Artist '{name}' added successfully.")
+        try:
+            check_artist = """
+            Select * from artists where name = %s;
+            """
+            cursor.execute(check_artist, (name,))
+            artist_exist = cursor.fetchone()
+            if artist_exist:
+                print(f"Artist '{name}' already exists.")
+                return
+            else:
+                sql_query = """
+                Insert into artists (name, country) values (%s,%s);
+                """
+                cursor.execute(sql_query, (name, country))
+                cursor.connection.commit()
+                print(f"Artist '{name}' added successfully.")
+        except Exception as ex:
+            print(f"Artist '{ex}' already exists")
+
 
 def add_album(album_name, release_year, artist_name):
     with DbConnection() as cursor:
@@ -53,6 +67,7 @@ def add_album(album_name, release_year, artist_name):
                 VALUES (%s,%s,(SELECT id FROM artists WHERE name = %s));
                 """
                 cursor.execute(sql_query, (album_name, release_year, artist_name))
+                cursor.connection.commit()
                 print(f"Album '{album_name}' added successfully.")
         except Exception as ex:
             print(f"Album '{ex}' already exists")
@@ -65,8 +80,8 @@ def add_song(title, duration, album_name, artist_name):
             AND artist_id = (SELECT id FROM artists WHERE name ilike %s);
             """
             cursor.execute(check_song,(f"%{title}%",f"%{artist_name}%"))
-            album_exists = cursor.fetchone()
-            if album_exists:
+            song_exists = cursor.fetchone()
+            if song_exists:
                 print (f"Song '{title}' by '{artist_name}' in the album '{album_name}' is already exists.")
                 return
             else:
@@ -76,6 +91,48 @@ def add_song(title, duration, album_name, artist_name):
                 (SELECT id FROM artists WHERE name = %s));
                 """
                 cursor.execute(sql_query, (title, duration, album_name, artist_name))
+                cursor.connection.commit()
                 print(f"Song '{title}' is added successfully.")
         except Exception as ex:
             print(f"Song '{ex}' is already exists")
+
+def update_song_title(old_name, name):
+    with DbConnection() as cursor:
+        update_query = """
+        Update songs
+        set title = %s 
+        where title = %s;
+        """
+        cursor.execute(update_query, (name,old_name))
+        cursor.connection.commit()
+        print(f"Song title '{old_name}' was updated to '{name}'.")
+
+def delete_song_by_title(name):
+    with DbConnection() as cursor:
+        update_query = """
+        DELETE FROM songs WHERE title = %s;
+        """
+        cursor.execute(update_query, (name,))
+        cursor.connection.commit()
+        print(f"Song '{name}' was deleted.")
+
+
+def get_artists_list():
+    with DbConnection() as cursor:
+        query = """
+        select * from artists;
+        """
+        cursor.execute(query)
+        return cursor.fetchall()
+
+def get_albums_by_band_name(name):
+    with DbConnection() as cursor:
+        query ="""
+        SELECT albums.name, albums.release_year, artists.name 
+            From albums
+            JOIN artists 
+			    ON artists.id = albums.artist_id
+            WHERE artists.name ILIKE %s;
+        """
+        cursor.execute(query, (f"%{name}%",))
+        return cursor.fetchall()
--- DB exercise/psycopg2/db/main.py
+++ DB exercise/psycopg2/db/main.py
@@ -1,44 +1,95 @@
 from datetime import datetime
-from db_queries import select_songs_from_album, select_songs_by_name, add_artist, add_album, add_song
+from db_queries import select_songs_from_album, select_songs_by_name, add_artist, add_album, add_song, \
+    update_song_title, delete_song_by_title, get_artists_list, get_albums_by_band_name
 
 while True:
     print("""
     Select action number from provided list:
-    1 - GET INFO about songs by album name
-    2 - GET INFO about songs by song name
-    3 - ADD artist,band
-    4 - ADD album for selected artist
-    5 - ADD song for selected artist and album
-    6 - EXIT program
+    1 - GET artists list
+    2 - GET albums list for selected singer/band
+    3 - GET INFO about songs by album name
+    4 - GET INFO about songs by song name
+    5 - ADD artist,band
+    6 - ADD album for selected artist
+    7 - ADD song for selected artist and album
+    8 - UPDATE song name
+    9 - DELETE song by name
+    10 - EXIT program
     """)
     action = int(input("Input an action number to start: "))
 
     if action == 1:
+        artists = get_artists_list()
+        for i in artists:
+            print(f'"{i[1]}", country {i[2]}')
+    elif action == 2:
+        name = input("Input singer/band name to get albums list: ")
+        albums = get_albums_by_band_name(name)
+        for i in albums:
+            print(f'"{i[0]}" album was released in {i[1]}')
+    elif action == 3:
         name = input("Input album name to get songs list: ")
         albums = select_songs_from_album(name)
-        for album in albums:
-            print(f'{album[1]}, {str(album[2])} - from "{album[3]}" album, by {album[4]}')
-    elif action == 2:
+        if not albums:
+            print("Nothing was found. Please, try again")
+        else:
+            for album in albums:
+                print(f'{album[1]}, {str(album[2])} - from "{album[3]}" album, by {album[4]}')
+    elif action == 4:
         name = input("Input song name to find all matching info: ")
         songs = select_songs_by_name(name)
-        for i in songs:
-            print(f'{i[1]}, {str(i[2])} - from "{i[3]}" album, by {i[4]}')
-    elif action == 3:
+        if not songs:
+            print("Nothing was found. Please, try again")
+        else:
+            for i in songs:
+                print(f'{i[1]}, {str(i[2])} - from "{i[3]}" album, by {i[4]}')
+    elif action == 5:
         band_name = input("Input band or artist name: ")
         band_country = input("Input band or artist country: ")
         add_artist(band_name, band_country)
-    elif action == 4:
+    elif action == 6:
         album_name = input("Input album name: ")
         band_name = input("Input band or artist name: ")
         release_year = int(input("Input album's release year: "))
         add_album(album_name, release_year, band_name)
-    elif action == 5:
+    elif action == 7:
         album_name = input("Input album name: ")
         band_name = input("Input band or artist name: ")
         song_name = input("Input song name: ")
         song_duration = input("Enter song duration (HH:MM:SS): ")
         dur_converted = datetime.strptime(song_duration, "%H:%M:%S").time()
         add_song(song_name, song_duration, album_name, band_name)
-    elif action == 6:
+    elif action == 8:
+        name = input("Find song name to update: ")
+        old_names = select_songs_by_name(name)
+        for i in old_names:
+            print(f'Song "{i[1]}", {str(i[2])} - from "{i[3]}" album, by {i[4]}')
+        while True:
+            old_name = input("Input found song name: ")
+            new_name = input("Input new song name: ")
+            if not old_name or not new_name:
+                print("Error: Song names cannot be empty. Try again.")
+                continue
+            else:
+                update_song_title(old_name, new_name)
+                break
+    elif action == 9:
+        name = input("Find song name to delete: ")
+        old_names = select_songs_by_name(name)
+        if not old_names:
+            print("Nothing was found. Please, try again")
+        else:
+            for i in old_names:
+                print(f'Song "{i[1]}", {str(i[2])} - from "{i[3]}" album, by {i[4]}')
+        while True:
+            name = input("Input found song name to delete it: ")
+            if not name:
+                print("Error: Song name cannot be empty. Try again.")
+                continue
+            else:
+                delete_song_by_title(name)
+                break
+
+    elif action == 10:
         print("Quit program")
         break
