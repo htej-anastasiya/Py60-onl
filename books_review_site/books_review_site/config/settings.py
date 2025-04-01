a130646810a0419e35af8d269d6e6118bfacde9a
anastasiya-khtsei <khtsei.anastasiya@gmail.com>
2025-04-01 10:25:16 +0200

    Books review: review and comments

--- books_review_site/books_review_site/config/settings.py
+++ books_review_site/books_review_site/config/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-ge0-=%mq-g0$mr@!bd=_q5kwc4qo0(8d49&m5fm@z(tyu&x0gd'
+
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
